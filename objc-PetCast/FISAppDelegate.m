ccf13728ad1e435029035f37e29f90c36edac05c
molgin <mollygingras@gmail.com>
2016-02-11 11:48:07 -0500

    practice casting with dogs

--- objc-PetCast/FISAppDelegate.m
+++ objc-PetCast/FISAppDelegate.m
@@ -1,10 +1,25 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISDog.h"
 
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISDog *dog = [[FISDog alloc] init];
+    NSLog(@"%@", [dog makeASound]);
+    NSLog(@"%@", [dog assaultTheMailman]);
+    
+    FISPet *dogAsPet = dog;
+    NSLog(@"%@", [dogAsPet makeASound]);
+    NSLog(@"%@", [dogAsPet eatSomething]);
+    NSLog(@"%@", [((FISDog *)dogAsPet) assaultTheMailman]);
+    
+    FISPet *fakeDog = [[FISPet alloc] init];
+    NSLog(@"%@", [((FISDog *)fakeDog) makeASound]);
+//    NSLog(@"%@", [((FISDog *)fakeDog) assaultTheMailman]);
+    
     return YES;
 }
 
